c9e754fdf6eaf70a30e5512831da57096c697bba
fujian <fujian@music.cn>
2018-12-12 20:42:31 +0800

    图片

--- hualaike/urls.py
+++ hualaike/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^',include('jindex.urls')),
+    url(r'^',views.index),
+    url(r'^list/&',include('jindex.urls')),
 ]
