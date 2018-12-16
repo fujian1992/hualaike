e74c7ab82355a10f99d305c812736114f6ddb18f
fujian <fujian@music.cn>
2018-12-16 16:17:48 +0800

    图片

--- hualaike/views.py
+++ hualaike/views.py
@@ -12,4 +12,54 @@ def news(request,parms):
     print '============'
     # print parms
     context ={}
-    return render(request,'news/2014/0304/'+parms,context)
+    return render(request,'news/'+parms,context)
+def company(request):
+    pass
+
+
+def case(request,parms):
+    """
+    案例展示
+    :param requset:
+    :return:
+    """
+    context={}
+    return render(request,'anli/'+parms,context)
+
+
+def about(request,parms):
+    """
+    关于我们
+    :param requset:
+    :return:
+    """
+    context={}
+    return render(request,'about/'+parms,context)
+
+
+def job(request,parms):
+    """
+    人才招聘
+    :param requset:
+    :return:
+    """
+    context={}
+    return render(request,'job/'+parms,context)
+
+def connection(request,parms):
+    """
+    联系我们
+    :param requset:
+    :return:
+    """
+    context={}
+    return render(request,'lianxi/'+parms,context)
+
+def product(request,parms):
+    """
+    产品
+    :param requset:
+    :return:
+    """
+    context={}
+    return render(request,'chanpin/'+parms,context)
